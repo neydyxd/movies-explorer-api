2a5b2b72307181f812fbc73584e520882d367286
Stepan Maksimov <neydyxd@yandex.ru>
2023-07-30 19:51:58 +0300

    Поменял мидлвар auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,11 +4,10 @@ const UnauthorizedError = require('../utils/errors/UnauthorizedError');
 const { NODE_ENV, JWT_SECRET } = process.env;
 
 module.exports = (req, res, next) => {
-  const { authorization } = req.headers;
-  if (!authorization || !authorization.startsWith('Bearer ')) {
+  const token = req.cookies.jwt;
+  if (!token) {
     throw new UnauthorizedError('Необходима авторизациb');
   }
-  const token = authorization.replace('Bearer ', '');
   let payload;
   try {
     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
